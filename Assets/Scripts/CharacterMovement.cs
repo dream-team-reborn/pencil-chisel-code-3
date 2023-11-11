89a4e38cf6a07fa1f3800e48eb2a9a2fa92e39fe
Matteo Stefanini <matteo.stefanini@digithub.co.uk>
2023-11-11 16:07:04 +0100

    feat(character): add character prefab

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,47 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
 
-    [SerializeField]
-    private float moveSpeed;
+    [SerializeField] private float moveSpeed;
 
-    [SerializeField]
-    private float jumpSpeed;
+    [SerializeField] private float jumpSpeed;
 
-    [SerializeField]
-    private GameObject floor;
+    [SerializeField] private float slidingGravityIncrease;
+    
+    [SerializeField] private GameObject flooringSurface;
 
-    private bool _isGrounded;
+    [SerializeField] private GameObject slidingSurface;
+
+    private Rigidbody _rigidbody;
+    private bool _isGrounded, _isSliding;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        HandleGravityChanges();
         HandlePlaneMovement();
         HandleJumpMovement();
     }
     
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == floor.name)
+        if (collision.gameObject.name == flooringSurface.name)
         {
             _isGrounded = true;
+        }
+        
+        if (collision.gameObject.name == slidingSurface.name)
+        {
+            _isSliding = true;
         }
     }
 
     void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.name == floor.name)
+        if (collision.gameObject.name == flooringSurface.name)
         {
             _isGrounded = false;
+        }
+        
+        if (collision.gameObject.name == slidingSurface.name)
+        {
+            _isSliding = false;
         }
     }
 
@@ -51,11 +65,23 @@ public class CharacterMovement : MonoBehaviour
         var verticalInput = Input.GetAxis("Vertical");
 
         if (horizontalInput == 0 && verticalInput == 0) return;
-        
+
         var movement = new Vector3(horizontalInput, 0, verticalInput);
+        var movementDelta = movement * (moveSpeed * Time.deltaTime);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
-        transform.Translate(movement * (moveSpeed * Time.deltaTime), Space.World);
-        ReduceVerticalMovement();
+        transform.Translate(movementDelta, Space.World);
+    }
+
+    private void HandleGravityChanges()
+    {
+        var gravity = Physics.gravity;
+        if ((!_isSliding || _isGrounded) && gravity.y < -9.8f)
+        {
+            Physics.gravity = new Vector3(0, -9.8f, 0);
+            return;
+        }
+
+        Physics.gravity = new Vector3(0, gravity.y * slidingGravityIncrease, 0);
     }
 
     private void HandleJumpMovement()
@@ -63,15 +89,7 @@ public class CharacterMovement : MonoBehaviour
         var jumpInput = Input.GetButtonDown("Jump");
 
         if (!jumpInput || !_isGrounded) return;
-        var movement = new Vector3(0, 1, 0);
-        transform.Translate(movement * (jumpSpeed * Time.deltaTime), Space.World);
+        var movement = new Vector3(0, jumpSpeed, 0);
+        _rigidbody.AddForce(movement);
     }
-
-    private void ReduceVerticalMovement()
-    {
-        //if (transform.y >= 100) {
-          //  transform.y -= transform.y * 1.10f;
-        //}
-    }
-
 }
